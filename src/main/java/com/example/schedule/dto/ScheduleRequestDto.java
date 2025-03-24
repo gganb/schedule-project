041d5aa43864af289fbe2e6e78ae06f7511601aa
gganb <gksql0801@naver.com>
2025-03-24 19:27:24 +0900

    Feat : ScheduleRequestDto 생성
    - com.example.schedule.dto 패키지에 Schedule 요청 DTO 클래스 추가
    - userName, task, password 필드를
    포함하며 Lombok @Getter 어노테이션을
    사용하여 게터 메서드 자동 생성

---
+++ src/main/java/com/example/schedule/dto/ScheduleRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.schedule.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ScheduleRequestDto {
+    private String userName;
+    private String task;
+    private String password;
+}
