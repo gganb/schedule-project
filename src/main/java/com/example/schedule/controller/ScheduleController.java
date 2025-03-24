3b158dc0de70e613957c6f12c058a43842ce7eff
gganb <gksql0801@naver.com>
2025-03-24 20:13:17 +0900

    Feat :  전체 글 조회 API 구현 - GET /api/schedule: 전체 일정 조회 API 추가 - Post API 주석 추가

--- src/main/java/com/example/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -6,10 +6,9 @@ import com.example.schedule.dto.ScheduleResponseDto;
 import com.example.schedule.service.ScheduleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/schedule")
@@ -21,10 +20,24 @@ public class ScheduleController {
         this.scheduleService = scheduleService;
     }
 
+    /**
+     * 일정 생성 API
+     *
+     * @param {@link scheduleRequestDto} 할 일 생성 요청 객체
+     * @return {@link ResponseEntity<ScheduleResponseDto>} JSON 응답
+     */
     @PostMapping
     public ResponseEntity<ScheduleResponseDto> createSchedule(
             @RequestBody ScheduleRequestDto scheduleRequestDto
-            ){
-       return new ResponseEntity<>(scheduleService.saveSchedule(scheduleRequestDto), HttpStatus.CREATED);
+    ) {
+        return new ResponseEntity<>(scheduleService.saveSchedule(scheduleRequestDto), HttpStatus.CREATED);
     }
+
+
+    @GetMapping
+    public List<ScheduleResponseDto> findAllTasks(){
+        return scheduleService.findAllTasks();
+    }
+
 }
+
