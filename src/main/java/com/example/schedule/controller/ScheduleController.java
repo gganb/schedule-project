91b26217ce0bbe44552eff7fb625976f938398b9
gganb <gksql0801@naver.com>
2025-03-24 20:49:10 +0900

    Feat :  단건 일정 조회 구현 - 단건 일정 조회를 위한 findScheduleById 메서드 구현 - id 값을 파라미터로 받아 해당 일정 데이터를 반환하도록 처리

--- src/main/java/com/example/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -23,8 +23,8 @@ public class ScheduleController {
     /**
      * 일정 생성 API
      *
-     * @param {@link scheduleRequestDto} 할 일 생성 요청 객체
-     * @return {@link ResponseEntity<ScheduleResponseDto>} JSON 응답
+     * @param  {@link scheduleRequestDto} 할 일 생성 요청 객체
+     * @return  {@link ResponseEntity<ScheduleResponseDto>} JSON 응답
      */
     @PostMapping
     public ResponseEntity<ScheduleResponseDto> createSchedule(
@@ -33,10 +33,25 @@ public class ScheduleController {
         return new ResponseEntity<>(scheduleService.saveSchedule(scheduleRequestDto), HttpStatus.CREATED);
     }
 
-
+    /**
+     * 일정 전체 조회 API
+     * @return {@link List<ScheduleResponseDto>}  JSON 응답
+     */
     @GetMapping
     public List<ScheduleResponseDto> findAllTasks(){
         return scheduleService.findAllTasks();
+    }
+
+    /**
+     * 일정 단건 조회 API
+     * @param {@link id}
+     * @return {@link ResponseEntity<ScheduleResponseDto>} JSON 응답
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<ScheduleResponseDto> findScheduleById(
+            @PathVariable Long id
+    ){
+        return new ResponseEntity<>(scheduleService.findScheduleById(id),HttpStatus.OK);
     }
 
 }
