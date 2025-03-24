03e5e18e3100a12a761f560f764733adb9beb94a
gganb <gksql0801@naver.com>
2025-03-24 20:14:16 +0900

    Feat :  전체 글 조회 로직 추가 - findAllTasks()를 통해 전체 일정 조회 기능 정의

--- src/main/java/com/example/schedule/service/ScheduleService.java
+++ src/main/java/com/example/schedule/service/ScheduleService.java
@@ -3,6 +3,10 @@ package com.example.schedule.service;
 import com.example.schedule.dto.ScheduleRequestDto;
 import com.example.schedule.dto.ScheduleResponseDto;
 
+import java.util.List;
+
 public interface ScheduleService {
     ScheduleResponseDto saveSchedule(ScheduleRequestDto scheduleRequestDto);
+
+    List<ScheduleResponseDto> findAllTasks();
 }
