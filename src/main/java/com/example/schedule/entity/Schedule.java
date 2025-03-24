733775bd5ac680fbec5a2048332c885741fd552d
gganb <gksql0801@naver.com>
2025-03-24 19:19:44 +0900

    Feat : 엔티티 생성 - 식별자, 작성자 명, 비밀번호, 할 일, 작성일/ 수정일로 필드를 추가함 - 할 일 저장을 위해 작성자명, 비밀번호, 할 일 필드만 사용하는 생성자를 별도로 선언함

---
+++ src/main/java/com/example/schedule/entity/Schedule.java
@@ -0,0 +1,37 @@
+package com.example.schedule.entity;
+
+
+import com.example.schedule.service.ScheduleService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+
+@Getter
+@AllArgsConstructor
+// id, 이름, 할일, 비번, 작성일, 수정일
+public class Schedule {
+
+    private Long id;
+    //  private Long userId;    // 외래키
+    private String userName;
+    private String password;
+    private String task;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    /**
+     * 글 작성을 위한 생성자
+     * @param userName
+     * @param password
+     * @param task
+     */
+    public Schedule(String userName, String task, String password){
+        this.userName = userName;
+        this.task = task;
+        this.password = password;
+    }
+
+
+}
