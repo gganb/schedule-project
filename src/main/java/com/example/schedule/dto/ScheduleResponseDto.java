a9ddb7bec8ee8648392b7bed057706f5ca4e4536
gganb <gksql0801@naver.com>
2025-03-24 19:30:32 +0900

    Feat : ScheduleResponseDto 생성
    - com.example.schedule.dto 패키지에 Schedule 응답 DTO 클래스 추가
    - @JsonFormat 어노테이션을 사용해 createdAt, updatedAt 필드를 "yyyy-MM-dd HH:mm:ss" 형식으로 출력하도록 출력 포맷 설정
    - timestamp 저장된 날짜를 localdatetime으로 변경해
    한국 시간이 출력되도록 설정함

---
+++ src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
@@ -0,0 +1,43 @@
+package com.example.schedule.dto;
+
+import com.example.schedule.entity.Schedule;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Getter
+public class ScheduleResponseDto {
+    private Long id;    // 글 번호
+    private String userName;
+    private String task;
+    // 1.한국시간변환
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+
+    public ScheduleResponseDto(Schedule schedule){
+        this.id = schedule.getId();
+        this.userName = schedule.getUserName();
+        this.task = schedule.getTask();
+        this.createdAt = convertToLocalDateTime(schedule.getCreatedAt());
+        this.updatedAt = convertToLocalDateTime(schedule.getUpdatedAt());
+
+    }
+
+    public ScheduleResponseDto(Long id, String userName, String task, Timestamp createdAt, Timestamp updatedAt) {
+        this.id = id;
+        this.userName = userName;
+        this.task = task;
+        this.createdAt = convertToLocalDateTime(createdAt);
+        this.updatedAt = convertToLocalDateTime(updatedAt);
+    }
+    private LocalDateTime convertToLocalDateTime(Timestamp timestamp) {
+        return timestamp != null ? timestamp.toLocalDateTime() : null;
+    }
+
+}
