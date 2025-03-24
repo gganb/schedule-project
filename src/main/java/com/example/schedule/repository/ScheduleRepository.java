4f5ead56a1d315fa02e97878c66cf69c1950eb63
gganb <gksql0801@naver.com>
2025-03-24 20:52:35 +0900

    Feat : findScheduleByIdOrElseThrow 메서드 추가 - 단건 일정 조회 시 null 값을 직접 처리하지 않도록 예외 처리를 포함한 메서드를 추가 - ScheduleRepository 인터페이스에 findScheduleByIdOrElseThrow(Long id) 메서드 선언

--- src/main/java/com/example/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedule/repository/ScheduleRepository.java
@@ -4,9 +4,12 @@ import com.example.schedule.dto.ScheduleResponseDto;
 import com.example.schedule.entity.Schedule;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ScheduleRepository {
     ScheduleResponseDto saveSchedule(Schedule schedule);
 
     List<ScheduleResponseDto> findAllTasks();
+
+    Schedule findScheduleByIdOrElseThrow(Long id);
 }
