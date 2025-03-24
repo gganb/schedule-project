c41423f44a0df8e5b4d5bb5b95a1d5a325798ad5
gganb <gksql0801@naver.com>
2025-03-24 19:16:08 +0900

    Fix : 테이블 수정 기존엔 외래키로 두 테이블을 연결시켰으나 우선 하나의 테이블로 관리해 모두 구현을 마친 후 분리시키기로 결정해 하나의 schedules 테이블에 작성자 정보, 작성글, 시간을 포함시키도록 변경함

--- src/main/resources/sql/schedule.sql
+++ src/main/resources/sql/schedule.sql
@@ -1,15 +1,12 @@
-create table users
-(
-    id       BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '작성자 id',
-    username VARCHAR(50) NOT NULL COMMENT '작성자 이름',
-    password VARCHAR(60) NOT NULL COMMENT '비밀번호'
-);
 
 create table schedules (
                            id BIGINT  AUTO_INCREMENT PRIMARY KEY COMMENT '일정 id',
-                           userId BIGINT not null COMMENT '작성자 id',
+                           username VARCHAR(50) NOT NULL COMMENT '작성자 이름',
+                           password VARCHAR(60) NOT NULL COMMENT '비밀번호',
                            task TEXT comment '할일',
-                           createdAt datetime default current_timestamp comment '작성된 시간',
-                           updatedAt datetime default current_timestamp on update current_timestamp comment '수정된 시간',
-                           foreign key (userId) references users(id)
+                           createdAt timestamp default current_timestamp comment '작성된 시간',
+                           updatedAt datetime default current_timestamp on update current_timestamp comment '수정된 시간'
+
+
 );
+SHOW CREATE TABLE schedules;
