60e6b61c561159ebdf7e1743f85610e18670cf31
gganb <gksql0801@naver.com>
2025-03-24 20:16:05 +0900

    Feat :  전체 글 조회 - 전체 일정 조회를 위한 findAllTasks 메서드 선언

--- src/main/java/com/example/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedule/repository/ScheduleRepository.java
@@ -3,6 +3,10 @@ package com.example.schedule.repository;
 import com.example.schedule.dto.ScheduleResponseDto;
 import com.example.schedule.entity.Schedule;
 
+import java.util.List;
+
 public interface ScheduleRepository {
     ScheduleResponseDto saveSchedule(Schedule schedule);
+
+    List<ScheduleResponseDto> findAllTasks();
 }
