0d598557365e6ee5c6ed408f6d999abf0b1dc239
gganb <gksql0801@naver.com>
2025-03-25 19:37:00 +0900

    Refactor : 구조 개선 및 주석, 메서드명 수정

--- src/main/java/com/example/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedule/repository/ScheduleRepository.java
@@ -5,25 +5,34 @@ import com.example.schedule.entity.Schedule;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 public interface ScheduleRepository {
+
+    // 일정 저장
     ScheduleResponseDto saveSchedule(Schedule schedule);
 
+    // 전체 일정 조회
     List<ScheduleResponseDto> findAllTasks();
 
+    // 사용자 이름으로 일정 목록 조회
     List<ScheduleResponseDto> findNameTasks(String userName);
 
+    // 날짜 기준 일정 조회
     List<ScheduleResponseDto> findScheduleByDate (Date updatedAt);
 
+    // id로 일정 조회
     Schedule findScheduleByIdOrElseThrow(Long id);
 
+    // 사용자 이름 + id로 단건 조회
     Schedule findScheduleByNameAndIdOrElseThrow(String userName, Long id);
 
+    // 일정 삭제
     int deleteSchedule(Long id);
 
-    String isEqualPassword(Long id);
+    // 해당 일정의 비밀번호 조회
+    String getPasswordById(Long id);
 
+    // 일정 수정 ( 이름, 할일)
     int updateSchedule(Long id, String userName, String task);
 
 }
