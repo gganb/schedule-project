3926b6ff1d35a8075b8a5b096f40baddd59e5901
gganb <gksql0801@naver.com>
2025-03-25 18:20:02 +0900

    fix(schema): updatedAt 컬럼 타입을 DATETIME에서 TIMESTAMP로 변경 - 스케줄 테이블의 수정일 컬럼 updatedAt의 타입을 DATETIME → TIMESTAMP로 변경 - 자동 시간 반영 및 표준화 목적 - 관련 쿼리 및 매핑 로직에서 Timestamp 타입 호환 확인 필요

--- src/main/resources/sql/schedule.sql
+++ src/main/resources/sql/schedule.sql
@@ -5,7 +5,7 @@ create table schedules (
                            password VARCHAR(60) NOT NULL COMMENT '비밀번호',
                            task TEXT comment '할일',
                            createdAt timestamp default current_timestamp comment '작성된 시간',
-                           updatedAt datetime default current_timestamp on update current_timestamp comment '수정된 시간'
+                           updatedAt timestamp default current_timestamp on update current_timestamp comment '수정된 시간'
 
 
 );
